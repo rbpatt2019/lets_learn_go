e7ec89a2b982c5d5291c780bdfb5f2d52fe6a01c
rbpatt2019 <rb.patterson.cross@gmail.com>
2021-12-15 09:55:06 +0000

    feat(main.go): introducing slices

--- main.go
+++ main.go
@@ -2,35 +2,35 @@ package main
 
 import "fmt"
 
-// Capacity vs length.
+// Slice expressions.
 func main() {
-	// First note:
-	// Empty slices from var are declared but not assigned. Hence, nil.
-	// Empty slices from make are declared and assigned.
-	// They are initialised with the zero value of their type.
-	var a []int
-	b := make([]int, 5, 10)
-	fmt.Println("a ", a, len(a), cap(a))
-	fmt.Println("b ", b, len(b), cap(b))
-	// Length is the nuber of elements in the array.
-	// Capacity is the number of elemets that could be in the array.
-	// If an append would make len > cap,
-	// a new slice is created with 2x cap (if cap < 1_024)
-	// or 1.25*cap (if cap > 1024).
-	c := []int{1, 2, 3, 4, 5}
-	b = append(b, c...)
-	fmt.Println("b ", b, len(b), cap(b))
-	b = append(b, 10)
-	fmt.Println("b ", b, len(b), cap(b))
-
-	// Given all this, what's the best way to declare a slice?
-	// 1) If it might stay nil (ie possibly no return value in a function), use:
-	// var x []int
-	// 2) If there are starting values, or the values won't change, use:
-	//  y := []int{1, 2, 3}
-	// 3) If you know the size, but not the values, use:
-	//  z := make([]int, 1, 5)
-	// Use a nonzero length when using a slice as a buffer
-	// If you 100% know the length, then use a non-zero length
-	// Oterwise, use a zero length and a non-zero capacity.
+	// Slicing is 0-indexed, open-ended.
+	x := []int{1, 2, 3, 4, 5}
+	fmt.Println(x[:2], x[2:])
+	// Assigning a slice expression to a var means the memory is shared.
+	y := x[:2]
+	y[0] = 100
+	fmt.Println(x, y)
+	// And things get really wonky with append,
+	// Because capacity is shared!
+	// Y has capacity 4, and shares memory with x.
+	// So when y is appended to, its third element goes into the same memory as the third element of x!
+	y = append(y, 200)
+	fmt.Println(x, y)
+	// To avoid, either never append to a substring,
+	// or use full slice expression notations.
+	// This contains a third position to indicate how much capacity is available to rhe sub-slice.
+	z := x[:2:2]
+	fmt.Println(z)
+	z = append(z, 500)
+	fmt.Println(x, z)
+	// And arrays can be converted to slices,
+	// but run ito the same memory sharing issues...
+	a := [4]int{1, 2, 3, 4}
+	b := a[:]
+	fmt.Printf("a: %T, %v\nb: %T, %v\n", a, a, b, b)
+	// For memory independence, use copy which, well copies the slice.
+	c := make([]int, 2)
+	copy(c, b)
+	fmt.Println(c)
 }
