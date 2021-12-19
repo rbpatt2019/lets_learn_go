f70c336acc5944bafc7689065b9bf7ca3bd12658
rbpatt2019 <rb.patterson.cross@gmail.com>
2021-12-19 20:04:20 +0000

    feat(main.go): shadowing

--- main.go
+++ main.go
@@ -2,38 +2,21 @@ package main
 
 import "fmt"
 
-// When you have related data that you want to group together,
-// Use a struct!
-// These are NOT objects/classes as they lack inheritance.
-type person struct {
-	name string
-	age  int
-}
-
-// Structs.
+// Shadowing.
+// A shadowing variable has the same name as a variable in a containing block.
 func main() {
-	// The zero value of a struct has all fields set to that field's 0-value.
-	var fred person
-	fmt.Println(fred)
-	// Structs can be created as literals.
-	// Fields not specified get their 0 value.
-	beth := person{
-		age:  30,
-		name: "Beth",
+	x := 10
+	if x > 5 {
+		// Uses x from the main block.
+		// As x has not been declared in this block.
+		// Prints 10.
+		fmt.Println(x)
+		// Creates a new variable constrained to the `if` block.
+		x := 5
+		// Prints 5.
+		fmt.Println(x)
 	}
-	fmt.Println(beth)
-	// Anonymous structs - creating a struct type without a name -
-	// are a thing, but they are mostly used for data marshalling
-	// and testing.
-	pet := struct {
-		name string
-		kind string
-	}{
-		name: "fido",
-		kind: "cat",
-	}
-	fmt.Printf("pet: %T, %v\n", pet, pet)
-	// Structs are ony comparable if al their fields are comparable
-	// And type conversions are only possible if all fields are in the same order, name, and type.
-	fmt.Println(beth == fred)
+	// Uses x from the main block.
+	// Prints 10.
+	fmt.Println(x)
 }
