24ec6a63708f45c591249deb028d9b16a32efce0
rbpatt2019 <rb.patterson.cross@gmail.com>
2022-01-06 17:00:57 +0000

    feat(main.go): functions as return values

--- main.go
+++ main.go
@@ -1,40 +1,78 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"os"
+	"strconv"
 )
 
-// Multiple return values.
+// Functions can also be defined as types...
+type opFuncType func(int, int) int
+
+// Functions are values.
 //
-// Unlike, say, python, these are actually multiple values,
-// not a single tuple that can be unpacked.
-//
-// Also, return values can be named.
-// When named, they are initilaised to their 0 value.
-// There is some debate as to the utility of this,
-// due to shadowing risks, and some potential odd return behaviour.
-// Your choice.
-func divAndRemainder(numerator, denominator int) (result, remainder int, err error) {
-	if denominator == 0 {
-		err = errors.New("Cannot divide by 0")
-		return result, remainder, err
-	}
-	result, remainder = numerator/denominator, numerator%denominator
-	return result, remainder, err
+// This means they can be passed around,
+// allowing for some fun behaviour.
+func add(i, j int) int {
+	return i + j
 }
 
-// Functions.
-//
-// Also, we see here a simple example of the idiomatic way to handle errors.
-// They are the last value returned from a a function,
-// and you retunr nil if there is no error.
+func sub(i, j int) int {
+	return i - j
+}
+
+func mul(i, j int) int {
+	return i * j
+}
+
+func div(i, j int) int {
+	return i / j
+}
+
+var opMap = map[string]opFuncType{
+	"+": add,
+	"-": sub,
+	"*": mul,
+	"/": div,
+}
+
+// Always check your errors!
 func main() {
-	result, remainder, err := divAndRemainder(5, 2)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	expressions := [][]string{
+		{"2", "+", "3"},
+		{"2", "-", "3"},
+		{"2", "*", "3"},
+		{"2", "/", "3"},
+		{"2", "%", "3"},
+		{"two", "+", "3"},
+		{"5"},
 	}
-	fmt.Println(result, remainder)
+	for _, exp := range expressions {
+		if len(exp) != 3 {
+			fmt.Println("Invalid expression:", exp)
+			continue
+		}
+
+		// Use strconv.Atoi to convert to an integer.
+		p1, err := strconv.Atoi(exp[0])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		op := exp[1]
+		opFunc, ok := opMap[op]
+		if !ok {
+			fmt.Println("Unsupported operator:", op)
+			continue
+		}
+
+		p2, err := strconv.Atoi(exp[2])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		result := opFunc(p1, p2)
+		fmt.Println(result)
+	}
 }
