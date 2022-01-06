de849b560e1f1e0c9046c636a30e378090f44e54
rbpatt2019 <rb.patterson.cross@gmail.com>
2022-01-06 16:41:59 +0000

    feat(main.go): multiple return values

--- main.go
+++ main.go
@@ -1,23 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
-// Variadic Inputs
+// Multiple return values.
 //
-// Must be last (or only) parameters.
-// Can be multiple values.
-func addTo(base int, vals ...int) []int {
-	out := make([]int, 0, len(vals))
-	for _, v := range vals {
-		out = append(out, base+v)
+// Unlike, say, python, these are actually multiple values,
+// not a single tuple that can be unpacked.
+//
+// Also, return values can be named.
+// When named, they are initilaised to their 0 value.
+// There is some debate as to the utility of this,
+// due to shadowing risks, and some potential odd return behaviour.
+// Your choice.
+func divAndRemainder(numerator, denominator int) (result, remainder int, err error) {
+	if denominator == 0 {
+		err = errors.New("Cannot divide by 0")
+		return result, remainder, err
 	}
-	return out
+	result, remainder = numerator/denominator, numerator%denominator
+	return result, remainder, err
 }
 
 // Functions.
+//
+// Also, we see here a simple example of the idiomatic way to handle errors.
+// They are the last value returned from a a function,
+// and you retunr nil if there is no error.
 func main() {
-	fmt.Println(addTo(3))
-	fmt.Println(addTo(3, 2, 3, 6))
-	// If you pass a slice as a variadic argument, you must add the trailing dots.
-	fmt.Println(addTo(3, []int{2, 3, 6}...))
+	result, remainder, err := divAndRemainder(5, 2)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(result, remainder)
 }
