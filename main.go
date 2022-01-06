9771b5fd9c55f3de66512afa62f41fda2e84d652
rbpatt2019 <rb.patterson.cross@gmail.com>
2022-01-06 15:15:35 +0000

    feat(main.go): introduce functions

--- main.go
+++ main.go
@@ -1,39 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-// Switch statements.
+// A function declaration!
+// All parameters must bet typed,
+// And consecutive parameters of the same type may be typed after the last one.
+// NB: Go does not have named or optional parameters!
+func div(numerator, denominator int) int {
+	if denominator == 0 {
+		return 0
+	}
+	return numerator / denominator
+}
+
+// Functions.
 func main() {
-	// Best used when there is a relationship between the variables.
-	// Cases DO NOT fall through,
-	// and empty cases do nothing.
-	words := []string{"a", "cow", "smile", "gopher", "octopus", "anthropologist"}
-	for _, v := range words {
-		switch size := len(v); size {
-		case 1, 2, 3, 4:
-			fmt.Println(v, "is a short word")
-		case 5:
-			fmt.Println(v, "is just right")
-		case 6, 7, 8, 9:
-		default:
-			fmt.Println(v, "is a long word")
-		}
-	}
-
-	// Switches can be cases on any expression (blank)
-	// Here's Fizzbuzz re-written with a blank switch.
-	for i := 1; i < 25; i++ {
-		switch {
-		case i%3 == 0 && i%5 == 0:
-			fmt.Println("FizzBuzz")
-		case i%3 == 0:
-			fmt.Println("Fizz")
-		case i%5 == 0:
-			fmt.Println("Buzz")
-		default:
-			fmt.Println(i)
-		}
-	}
+	results := div(5, 2)
+	fmt.Println(results)
 }
