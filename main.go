244ec0fd1b59036020113ec68dd640e0af4e82a4
rbpatt2019 <rb.patterson.cross@gmail.com>
2022-01-06 16:25:30 +0000

    feat(main.go): variadic arguments

--- main.go
+++ main.go
@@ -2,19 +2,22 @@ package main
 
 import "fmt"
 
-// A function declaration!
-// All parameters must bet typed,
-// And consecutive parameters of the same type may be typed after the last one.
-// NB: Go does not have named or optional parameters!
-func div(numerator, denominator int) int {
-	if denominator == 0 {
-		return 0
+// Variadic Inputs
+//
+// Must be last (or only) parameters.
+// Can be multiple values.
+func addTo(base int, vals ...int) []int {
+	out := make([]int, 0, len(vals))
+	for _, v := range vals {
+		out = append(out, base+v)
 	}
-	return numerator / denominator
+	return out
 }
 
 // Functions.
 func main() {
-	results := div(5, 2)
-	fmt.Println(results)
+	fmt.Println(addTo(3))
+	fmt.Println(addTo(3, 2, 3, 6))
+	// If you pass a slice as a variadic argument, you must add the trailing dots.
+	fmt.Println(addTo(3, []int{2, 3, 6}...))
 }
