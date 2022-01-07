3e92bb0cb5ea015571b7891ef613060eb0f15388
rbpatt2019 <rb.patterson.cross@gmail.com>
2022-01-07 19:44:00 +0000

    feat(main.go): call by value
    Run with `--no-verify`.

--- main.go
+++ main.go
@@ -1,57 +1,51 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"log"
-	"os"
-)
+import "fmt"
 
-// Defer
-//
-// In go, resources cleanup is handled with the defer keword.
-// These delay calling until the surrounding function exits,
-// and guarantee that certain code runs, no matter what.
-func Contents(filename string) (string, error) {
-	// Then check that the file handle is valid.
-	// If it is, we defer closing it.
-	f, err := os.Open(filename)
-	if err != nil {
-		return "", err
-	}
-	defer f.Close()
-
-	// Introducing idiomatic file reading in Go
-	// First create a buffer of bytes.
-	// Then read to that buffer.
-	// Read returns the number of bytes read, and an err.
-	// If err is io.EOF, then the file has read successfully.
-	var result []byte
-	buf := make([]byte, 2048)
-	for {
-		n, err := f.Read(buf)
-		result = append(result, buf[:n]...)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return "", err
-		}
-	}
-	return string(result), nil
+type person struct {
+	name string
 }
 
-func main() {
-	// Os.Args is a slice containing parameters passed to the script
-	// Os.Args[0] is the filename.
-	if len(os.Args) < 2 {
-		log.Fatal("No file specified")
-	}
+func modifyNot(i int, s string, p person) {
+	i *= 2
+	s = "Nope"
+	p.name = "Bob"
+}
 
-	// Once we know we have a file we open it.
-	results, err := Contents(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
+func modifyMostly(m map[int]string, l []int) {
+	m[2] = "yes"
+	m[3] = "totally"
+	delete(m, 1)
+
+	for k, v := range l {
+		l[k] = v * 2
 	}
-	fmt.Println(results)
+	l = append(l, 10)
+}
+
+// Go is call by value
+//
+// When you supply a variable for a parameter,
+// Go always makes a copy.
+// This effectively makes variables immutable,
+// unless pointers are used (more on that later).
+//
+// This also means that maps and slices behave slightly differently,
+// as they are implemented with pointers.
+func main() {
+	p := person{}
+	i := 2
+	s := "Yes"
+	modifyNot(i, s, p)
+	fmt.Println(i, s, p) // None will change.
+
+	m := map[int]string{
+		1: "a",
+		2: "b",
+	}
+	l := []int{1, 2, 3}
+	modifyMostly(m, l)
+	fmt.Println(m, l)
+	// Maps are fully mutable (pointer)
+	// Slices cannot be lengthened.
 }
