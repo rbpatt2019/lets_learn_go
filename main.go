3ac5e4df2fea8f1af897758392edbd677c6951a2
rbpatt2019 <rb.patterson.cross@gmail.com>
2022-01-15 17:31:11 +0000

    feat(main.go): updating pointers
    Run with --no-verify.

--- main.go
+++ main.go
@@ -2,37 +2,32 @@ package main
 
 import "fmt"
 
-type Person struct {
-	FirstName string
-	LastName  string
+// Since the copy of a pointer still points to the same memory,
+// this updates the contents.
+func update(px *int) {
+	*px = 20
+}
+
+// Call by value - so this changes the copy, not the original.
+// This actually fails lints!
+func failUpdate(px *int) {
+	x := 20
+	px = &x
 }
 
 // Pointers
 //
-// A pointer is simply a variable whose contents are the address
-// where another variable is stored.
+// Since go is call by value,
+// a passed pointer is copied.
+// However, this means that nil pointers cannot be updated by functions.
 //
-// Its zero value is nil.
+// Additionally,
+// if you want the assigned value of a pointer to be there after exiting a function,
+// then you must dereference the pointer on assignment.
 func main() {
-	x := "Hello"
-	// & os the address operator.
-	// It returns a pointer to the variable.
-	pointerToX := &x
-	// * is the indirection operator.
-	// It returns the value a pointer points to.
-	// This is called dereferencing.
-	// Dereferencing a nil pointer panics.
-	fmt.Println(pointerToX)
-	fmt.Println(*pointerToX)
-	// New returns a pointer to the zero value of the indicated type.
-	y := new(int)
-	fmt.Println(y, *y)
-	// Thought that's not particularly idiomatic.
-	// For primitive types, declare a variable then point to it.
-	var z string
-	a := &z
-	fmt.Println(a)
-	// For structs, derefence a struct literal to create a pointer.
-	b := &Person{}
-	fmt.Println(b)
+	x := 10
+	failUpdate(&x)
+	fmt.Println(x)
+	update(&x)
+	fmt.Println(x)
 }
