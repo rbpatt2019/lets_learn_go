0fac65dcbb402501533915ef213790b4ef42949a
rbpatt2019 <rb.patterson.cross@gmail.com>
2022-01-28 14:46:29 +0000

    feat(main.go): introduce iota
    Run with --no-verify

--- main.go
+++ main.go
@@ -4,26 +4,24 @@ import (
 	"fmt"
 )
 
-type Adder struct {
-	start int
-}
+type MailCategory int
 
-func (a Adder) AddTo(val int) int {
-	return a.start + val
-}
+// Generally, use iota for interaml purposes only,
+// where constants are referred to by name rather than value.
+//
+// Or put another way:
+// only use iota when you want to differentiate between values,
+// but don't care what those values are.
+// If the actual value matters,
+// specify it.
+const (
+	Uncategorised MailCategory = iota
+	Personal
+	Spam
+	Social
+	Advertisements
+)
 
 func main() {
-	// Method value
-	// Assigning a method to a variable
-	// In this case, the method is bound to a specific object.
-	myAdder := Adder{start: 10}
-	f1 := myAdder.AddTo
-	fmt.Println(f1(10))
-
-	// Method Expression
-	// Assigning a method from a literal to a variable
-	// In this case, it is not bound to an instance,
-	// and must be passed an instance of the appropriate type as the first parameter.
-	f2 := Adder.AddTo
-	fmt.Println(f2(myAdder, 10))
+	fmt.Println(Uncategorised)
 }
