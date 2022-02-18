9829dd0fd58a5605e0b9d4360a159c129c18df66
rbpatt2019 <rb.patterson.cross@gmail.com>
2022-02-18 14:34:53 +0000

    feat(main.go): interfaces
    Type safe duck typing

--- main.go
+++ main.go
@@ -2,35 +2,45 @@ package main
 
 import "fmt"
 
-type Employee struct {
-	Name string
-	ID   string
+type LogicProvider struct{}
+
+func (lp LogicProvider) Process(data string) string {
+	// Do the things.
+	return fmt.Sprintf("New string: %s", data)
 }
 
-func (e Employee) Description() string {
-	return fmt.Sprintf("%s (%s)", e.Name, e.ID)
-}
-
-// Embedding
+// Interfaces
 //
-// Is not inheritance!
-// Employee has no name assigned to it,
-// Making it an embedded field.
-// Any method declared on an embedded field
-// is promoted to the containing struct.
-type Manager struct {
-	Employee
-	Reports []Employee
+// Type safe duck typing.
+// Interfaces specify the methods a struct needs to have.
+// But, they do not care how those methods are provided.
+// Here, LogicProvider meets the interface.
+// Yet, if we changed to an utterly un-related provider,
+// it would still meet the interface as long as there was
+// a Process method.
+type Logic interface {
+	Process(data string) string
 }
 
-// Which makes the following valid!
+// Essentially,
+// only the client knows about the interface.
+// Thus, we get the flexibility of duck typing
+// with the bonus of type checking!
+type Client struct {
+	L Logic
+}
+
+func (c Client) Program(data string) string {
+	return c.L.Process(data)
+}
+
 func main() {
-	m := Manager{
-		Employee: Employee{
-			Name: "Bob",
-			ID:   "123",
-		},
-		Reports: []Employee{},
+	c := Client{
+		L: LogicProvider{},
 	}
-	fmt.Println(m.ID)
+
+	// In the real world,
+	// we'd get this data from somewhere rather than hard-code it.
+	data := "Hello World"
+	c.Program(data)
 }
