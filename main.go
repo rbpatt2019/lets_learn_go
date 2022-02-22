f7a2c7457b7ba2b84707db77f1912137bf3f7658
rbpatt2019 <rb.patterson.cross@gmail.com>
2022-02-22 14:53:54 +0000

    feat(main.go): custom errors
    Run with --no-verify to include examples.

--- main.go
+++ main.go
@@ -1,28 +1,24 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"os"
+type Status int
+
+// Define an enumeration to hold the status codes.
+const (
+	InvalidLogin Status = iota + 1
+	NotFound
 )
 
-// Basic error handling.
-// Create by calling `errors.New`.
-// Should not be capitalised, and should not end with punctuation.
-// Return nil if successful.
-func calcRemainderAndMod(numerator, denominator int) (int, int, error) {
-	if denominator == 0 {
-		return 0, 0, errors.New("denominator is 0")
-	}
-	return numerator / denominator, numerator % denominator, nil
+// Define a struct to hold the error information.
+type StatusErr struct {
+	Status  Status
+	Message string
 }
 
-// Check for errors by comparing err to nil.
-func main() {
-	r, m, err := calcRemainderAndMod(1, 2)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	fmt.Println(r, m)
+// And a method to guarantee we neet the interface.
+func (se StatusErr) Error() string {
+	return se.Message
 }
+
+// Now, you can return errors with more information about the failure.
+// Even when implementing a custom error type,
+// always return the standard error.
