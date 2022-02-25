49ecaabef48f3b566f5fd5fd60821e74f76c20e8
rbpatt2019 <rb.patterson.cross@gmail.com>
2022-02-25 12:24:24 +0000

    feat(main.go): panic and recover

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
-import (
-	"errors"
-)
+import "fmt"
 
-// Use `errors.As` to check if any error in the error chain matches a specific type.
-// It takes the error and a pointer to a variable of the desired error type.
-// Then, if the error is present, it is assigned to the pointer.
+// Recovers must in deferred functions,
+// as only defers run after panics.
+// Idiomatically,
+// It would be better to check for div by 0 and report the appropriate error.
+func div60(i int) {
+	defer func() {
+		if v := recover(); v != nil {
+			fmt.Println(v)
+		}
+	}()
+	fmt.Println(60 / i)
+}
+
+// Panic/recover should be used more or less only to:
+// 1) log system failures to monitoring software.
+// 2) prevent panics from leaking your API by turning them into errors.
 func main() {
-	err := SomethingThatErrors()
-    var myErr MyErr
-    if errors.As(err, &myErr) {
-        // Do things with myErr
-    }
+	for _, val := range []int{1, 0, 2} {
+		div60(val)
+	}
 }
