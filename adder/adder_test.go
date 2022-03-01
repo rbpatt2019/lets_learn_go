8425cb8967efebe8c428224099b9c5c6cf955079
rbpatt2019 <rb.patterson.cross@gmail.com>
2022-03-01 15:01:01 +0000

    feat(adder_test.go): TestMain
    Run with no-verify. Checks fail with undeclared variable if the whole
    package is not linted in one go.

--- adder/adder_test.go
+++ adder/adder_test.go
@@ -1,16 +1,35 @@
 package adder
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+)
 
-// Test functions
-// Always start with Test, followed by the name of the function.
-// If the tested function is unexported, then an underscore is frequently used.
-// They are part of the package as the code so tha tthey can access unexported
-// functions, code, etc.
-// They take one parameter `t *testing.T`.
+var testTime time.Time
+
+// TestMain is run once per package to provide common test setup.
+// It (basically) always has the structure indicated below.
+// It's most useful for either setting up data in an external repository
+// ie a database,
+// or for package-level variable initialisation (as here).
+// But you should really try to avoid the latter anyways!
+func TestMain(m *testing.M) {
+	fmt.Println("Test setup")
+	testTime = time.Now()
+
+	fmt.Println("Run tests")
+	exit := m.Run()
+
+	fmt.Println("Test cleanup")
+	os.Exit(exit)
+}
+
 func Test_addNumbers(t *testing.T) {
 	result := addNumbers(2, 3)
 	if result != 5 {
 		t.Error("incorrect result: expected 5, got", result)
 	}
+	fmt.Println(testTime)
 }
